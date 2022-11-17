70afc21267382b90069fb833571e8e71b7c685be
tr-brain-com <118043046+tr-brain-com@users.noreply.github.com>
2022-11-17 23:34:41 +0300

    Add files via upload

--- api/route/health.py
+++ api/route/health.py
@@ -3,5 +3,9 @@ from fastapi import APIRouter
 routes = APIRouter()
 
 @routes.get("/health")
-async def health():
-    return {"status":"UP"}
+async def health_check():
+    return {
+        "status": "UP"
+    }
+
+
